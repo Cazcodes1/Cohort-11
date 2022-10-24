efeff1ca90d57f613a6eb42d3cf47385369d14fd
Cazza73 <carolinew811@gmail.com>
2022-10-24 19:54:34 +0100

    added styling to bmi

--- Session 10/BMI calculator/script.js
+++ Session 10/BMI calculator/script.js
@@ -1,11 +1,24 @@
-function calcBMI() {
-  let bmi = null;
-  weight = document.getElementById("weight").value;
-  height = document.getElementById("height").value;
-  result = document.getElementById("result");
+let heightInput = document.getElementById("height");
+let weightInput = document.getElementById("weight");
+let calculateButton = document.getElementById("calculate");
+let result = document.getElementById("result");
+let statement = document.getElementById("result-statement");
+var BMI, height, weight;
 
-  height = height / 100;
-  bmi = weight / (height * height);
-  bmi = Math.round(bmi * 100) / 100;
-  result.innerHTML = bmi;
+function calcBMI() {
+  height = heightInput.value;
+  weight = weightInput.value;
+  BMI = weight / height ** 2;
+  result.innerText = BMI;
+
+  if (BMI < 18.5) {
+    statement.innerText = "Your BMI falls within the underweight range";
+  } else if (BMI > 18.5 && BMI < 24.9) {
+    statement.innerText =
+      "Your BMI falls within the normal or healthy weight range";
+  } else if (BMI > 25 && BMI < 29.9) {
+    statement.innerText = "Your BMI falls within the overweight range";
+  } else {
+    statement.innerText = "Your BMI falls within the obese range";
+  }
 }
