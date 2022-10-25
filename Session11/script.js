849ef1cfd9b689fd1d3e3e4789654268892f3b0e
Cazza73 <carolinew811@gmail.com>
2022-10-25 14:20:39 +0100

    shoopping list added

---
+++ Session11/script.js
@@ -0,0 +1,25 @@
+let btn = document.getElementById("btn");
+let txt = document.getElementById("txt");
+let ul = document.getElementById("ul");
+let list = [];
+
+btn.addEventListener("click", addValue);
+
+function addValue() {
+  let item = txt.value;
+
+  if (list.includes(item)) {
+    alert("This item is already on the list!");
+  } else {
+    list.push(item);
+  }
+  txt.value = "";
+
+  let output = "";
+
+  for (i = 0; i < list.length; i++) {
+    output += "<li>" + list[i] + "</li>";
+  }
+
+  ul.innerHTML = output;
+}
