1621ff17a504b62223a0c9db9f0b5b5bb6eb6892
Cazza73 <carolinew811@gmail.com>
2022-10-29 17:08:03 +0100

    music added to strictly page

--- Session 4/script.js
+++ Session 4/script.js
@@ -5,8 +5,7 @@ btnYes.addEventListener("onclick", communicateYes);
 btnNo.addEventListener("onclick", communicateNo);
 
 function communicateYes() {
-  document.getElementById("answer").innerText =
-    "Amazing, sign up with the button below!!";
+  document.getElementById("answer").innerText = "Amazing, let's get started!!";
   document.getElementById("stay-btn").style.display = "block, center";
   document.getElementById("leave-btn").style.display = "none";
   document.getElementById("answer2").innerText = "";
@@ -20,3 +19,29 @@ function communicateNo() {
   document.getElementById("stay-btn").style.display = "none";
   document.getElementById("answer").innerText = "";
 }
+
+let btnOn = document.getElementById("click", playMusic());
+let btnOff = document.getElementById("click", pauseMusic());
+let volumeChange = document.getElementById("volume-bar");
+
+let volume = 5;
+
+function playMusic() {
+  document.getElementById("audio").play();
+}
+
+playMusic();
+
+function stopMusic() {
+  document.getElementById("audio").pause();
+}
+
+stopMusic();
+
+function changeVolume() {
+  volumeChange.addEventListener("input", (e) => {
+    audio.volume = e.volumeChange.value / 10;
+  });
+}
+
+changeVolume();
