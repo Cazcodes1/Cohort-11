9004dfb9a8be40207b323c455f6e16201d958ec9
Cazza73 <carolinew811@gmail.com>
2022-11-23 18:59:43 +0000

    updated form

---
+++ Ol_session3/JS/data.js
@@ -0,0 +1,22 @@
+const shoppingItems = [
+  {
+    item: "Milk",
+    price: 1.99,
+    category: "Dairy",
+  },
+  {
+    item: "Mars Bar",
+    price: 1.49,
+    category: "Chocolate",
+  },
+  {
+    item: "Coca Cola",
+    price: 1.99,
+    category: "Drinks",
+  },
+  {
+    item: "Best of Both",
+    price: 1.0,
+    category: "Bakery",
+  },
+];
